38900e0fd385b4ce8728265c42f5a6e2dbe2907d
RickBeniers <Beniersrick@gmail.com>
2024-05-28 03:21:58 +0200

    tried to add dependencies and css. 28-05-2024, 0321.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>I Game Simulator</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        <div class="flex-container">
+            <div>1</div>
+            <div>2</div>
+        </div>
+    </header>
+</body>
+</html>
