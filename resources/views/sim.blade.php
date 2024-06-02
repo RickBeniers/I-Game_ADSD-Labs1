ee03ed846eed53f255cb666bcbdbef97bb8d9132
RickBeniers <Beniersrick@gmail.com>
2024-06-03 01:42:12 +0200

    expanded sim.blade.php. 03-06-2024, 0142.

--- resources/views/sim.blade.php
+++ resources/views/sim.blade.php
@@ -1,10 +1,24 @@
 <?php include"home.blade.php"; ?>
-<link rel=stylesheet type=text/css href=/dashboard/projects/Igame/Igame/resources/css/sim.css?v=2>
+<link rel=stylesheet type=text/css href=/dashboard/projects/Igame/Igame/resources/css/sim.css?v=3>
         <main class=simDimentions>
             <div class=grid-container2>
-                <div class=grid-item3></div>
-                <div class=grid-item4></div>
-                <div class=grid-item5></div>
+                <div class="grid-item3 grid-container3">
+                    <div class=grid-item6></div>
+                    <div class=grid-item7></div>
+                </div>
+                <div class=grid-item4>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class=grid-item5>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </main>
     </body>
