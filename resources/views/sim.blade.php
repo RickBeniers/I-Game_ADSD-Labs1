e9f5d885cec51d0e05b9565c47ebaa346d4a8495
RickBeniers <Beniersrick@gmail.com>
2024-06-04 16:10:11 +0200

    played around with css grid and flex box. 04-06-2024, 1610.

--- resources/views/sim.blade.php
+++ resources/views/sim.blade.php
@@ -1,8 +1,10 @@
-<?php include"home.blade.php"; ?>
-<link rel=stylesheet type=text/css href=/dashboard/projects/Igame/Igame/resources/css/sim.css?v=3>
+<?php include"home.blade.php"; 
+/* link href tag may be incorrect, change href path to reflect local path.*/
+?>
+<link rel=stylesheet type=text/css href=/dashboard/projects/Igame/resources/css/sim.css?v=4>
         <main class=simDimentions>
             <div class=grid-container2>
-                <div class="grid-item3 grid-container3">
+                <div class="grid-item3 flex-container3">
                     <div class=grid-item6></div>
                     <div class=grid-item7></div>
                 </div>
