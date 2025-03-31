6c7c90a3c1fa45a19537f319601c0c00d9dd1bc6
RickBeniers <Beniersrick@gmail.com>
2025-03-31 23:25:09 +0200

    Eindoplevering DevOps 2025. 31-03-2025, 2325.

---
+++ tests/Unit/ObjectCreationOrderHistoryViewTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ObjectCreationOrderHistoryViewTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+
+      I forgot that the IGame project only had HTML & CSS
+      Unable to test unittests as designed in SDD.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
